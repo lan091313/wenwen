e3ce9825b96670cf34f38d3e077f4d272c2d1e31
lan091313 <3392379626@qq.com>
2019-10-11 21:12:00 +0800

    第二次修改

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require("gulp");
-
+const sass = require("gulp-sass")
 
 
 
@@ -9,4 +9,10 @@ gulp.task("watchall",async()=>{
 		gulp.src("*.html")
 		.pipe(gulp.dest("d:\\phpstudy\\www\\wenwen"))
 	});
+
+	// gulp.watch("sass/**/*",async()=>{
+	// 	gulp.src("sass/**/*")
+	// 	.pipe(sass());
+	// 	.pipe(gulp.dest("d:\\phpstudy\\www\\wenwen\\css"))
+	// })
 })
