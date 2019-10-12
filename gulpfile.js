635dce628079d6c8f7a59aa9dc3352a8ef45bb7f
lan091313 <3392379626@qq.com>
2019-10-12 21:08:02 +0800

    第三次更新

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 const gulp = require("gulp");
-const sass = require("gulp-sass")
-
+const sass = require("gulp-sass");
+// const uglify = require("gulp-uglify");
+const minifycss = require("gulp-minify-css");
+// const babel = require("gulp-babel");
+// const es2015Preset = require("babel-preset-es2015");
 
 
 
@@ -10,9 +13,29 @@ gulp.task("watchall",async()=>{
 		.pipe(gulp.dest("d:\\phpstudy\\www\\wenwen"))
 	});
 
-	// gulp.watch("sass/**/*",async()=>{
-	// 	gulp.src("sass/**/*")
-	// 	.pipe(sass());
-	// 	.pipe(gulp.dest("d:\\phpstudy\\www\\wenwen\\css"))
-	// })
+	gulp.watch("img",async()=>{
+		gulp.src("img/**/*")
+		.pipe(gulp.dest("d:\\phpstudy\\www\\wenwen\\img"))
+	});
+
+	gulp.watch("sass/**/*",async()=>{
+		gulp.src("sass/**/*")
+		.pipe(sass())
+		.pipe(gulp.dest("d:\\wenwen\\css"))
+	})
+	
+
+     gulp.watch("css/*.css",async ()=>{
+        gulp.src("css/*.css")
+		.pipe(minifycss())
+        .pipe(gulp.dest("d:\\phpstudy\\www\\wenwen\\css"));
+    });
+
+    //  gulp.watch("js/*.js",async ()=>{
+    //     gulp.src("js/*.js")
+    //     .pipe(babel({presets:[es2015Preset]}))
+    //     .pipe(uglify())
+    //     .pipe(gulp.dest("d:\\phpstudy\\www\\wenwen\\js"));
+    // });
+
 })
